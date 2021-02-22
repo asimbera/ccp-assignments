d8246340294c4442889a47816885b003007f5027
Asim Bera <asimbera@outlook.in>
2021-02-22 18:07:57 +0530

    Update 06.c

--- data_structure/assignment_1/06.c
+++ data_structure/assignment_1/06.c
@@ -8,35 +8,24 @@ int main()
   scanf("%d", &l);
 
   for (int i = 0; i < l; i++)
-  {
-    printf("Elem (%d): ", i);
     scanf("%d", &arr_a[i]);
-  }
 
   p = l % 2 == 0 ? l / 2 : (l + 1) / 2;
   q = l - p;
 
   for (int i = 0; i < p; i++)
-  {
     arr_b[i] = arr_a[i];
-  }
 
   for (int i = 0; i < q; i++)
-  {
     arr_c[i] = arr_a[i + p];
-  }
 
   printf("\nElem of b array: \n");
   for (int i = 0; i < p; i++)
-  {
     printf("%d ", arr_b[i]);
-  }
 
   printf("\nElem of c array: \n");
   for (int i = 0; i < q; i++)
-  {
     printf("%d ", arr_c[i]);
-  }
 
   return 0;
 }
