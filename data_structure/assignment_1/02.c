a67cf487d90f1f7dea3cd4fb4e68a88e6398bb64
Asim Bera <asimbera@outlook.in>
2021-02-22 18:04:45 +0530

    Update 02.c

--- data_structure/assignment_1/02.c
+++ data_structure/assignment_1/02.c
@@ -8,17 +8,11 @@ int main()
   scanf("%d", &l);
 
   for (int i = 0; i < l; i++)
-  {
-    printf("Elem (%d): ", i);
     scanf("%d", &arr[i]);
-  }
 
   printf("Elem in rev order: \n");
   for (int i = l - 1; i >= 0; i--)
-  {
     printf("%d, ", arr[i]);
-  }
-  printf("\n");
 
   return 0;
 }
